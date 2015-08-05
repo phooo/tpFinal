bcbd44d484cf370763efbac547ab13683b6c83ba
phooo <pulideleone@gmail.com>
2015-08-04 21:57:31 -0300

    parte uno

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(function() {
-  
+$(document).ready(function(){
+	$('.carrousel').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  centerMode: true,
+	  fade: true,
+	  cssEase: 'linear',
+	  arrows: false
+	});
 });
